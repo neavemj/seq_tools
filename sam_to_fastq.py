f4846e417d8e724ff41f1f9e2672544a79d02676
neavemj <matthewjneave1@gmail.com>
2018-07-20 12:51:46 +1000

    convert sam to fastq

---
+++ sam_to_fastq.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# python 3
+
+# script to convert a sam file to fastq
+# Matthew J. Neave 20.07.2018 <matthewjneave1@gmail.com>
+
+# library imports
+
+import sys
+import argparse
+from Bio.SeqIO.QualityIO import FastqGeneralIterator # requires Biopython
+from Bio import SeqIO
+
+# use argparse to grab command line arguments
+
+parser = argparse.ArgumentParser("convert sam file to forward and reverse fastq files\n")
+
+parser.add_argument('-s', '--sam_file', type = str,
+                    nargs = "?", help = "sam file to convert")
+parser.add_argument('-o', '--output_prefix', type = str,
+                    nargs = "?", help = "a name to prefix the output files")
+
+# if no args given, print help and exit
+
+if len(sys.argv) == 1:
+    parser.print_help(sys.stderr)
+    sys.exit(1)
+
+args = parser.parse_args()
+
+# check required arguments are provided
+
+if args.sam_file is None or args.output_prefix is None:
+    print("\n** required input missing\n"
+          "** a sam file and output prefix are all required\n")
+    parser.print_help(sys.stderr)
+    sys.exit(1)
+
+sam_fl = args.sam_file
+output_prefix = args.output_prefix
+
+# create handles for the output
+
+forward_handle = open(args.output_prefix + ".R1.fastq", "w")
+reverse_handle = open(args.output_prefix + ".R2.fastq", "w")
+orphan_handle = open(args.output_prefix + ".single.fastq", "w")
+
+print("Scanning sam file and building list of names...")
+paired_set = set()
+single_set = set()
+
+with open(sam_fl) as fl:
+    for line in fl:
+        line = line.strip()
+        if line.startswith("@"):
+            continue
+        name = line.split()[0]
+        if name in single_set:
+            paired_set.add(name)
+            single_set.remove(name)
+        else:
+            single_set.add(name)
+
+print("detected {} paired reads and {} single reads".format(len(paired_set), len(single_set)))
+
+print("writing output files")
+
+with open(sam_fl) as fl:
+
+    paired = False
+    for line in fl:
+        line = line.strip()
+        if line.startswith("@"):
+            continue
+        cols = line.split("\t")
+        name = cols[0]
+        seq = cols[9]
+        qual = cols[10]
+
+        if name in paired_set:
+
+forward_handle.write("@%s\n%s\n+\n%s\n" % (title, seq, qual))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

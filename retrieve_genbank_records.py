7d8227159317a1b6e0cca7235424bdab99b0495a
neavemj <matthewjneave1@gmail.com>
2018-12-19 11:47:52 +1100

    script to download genbank records

--- retrieve_genbank_records.py
+++ retrieve_genbank_records.py
@@ -6,15 +6,18 @@
 # requires biopython
 from Bio import Entrez
 from Bio import SeqIO
+import argparse
+import sys
 
 Entrez.email = "matthewjneave1@gmail.com"
 
 # use argparse to grab command line arguments
 
-parser = argparse.ArgumentParser("download complete genbank records given file containing NCBI accession numbers (one per line). "
-                                 "Each record will be written into a separate file named by the accession number + .gb"
+parser = argparse.ArgumentParser("Given file containing NCBI accession numbers (one per line), download complete genbank records.\n"
+                                 "Each record will be written into a separate file named by the accession number + .gb.\n"
+                                 "To run directly without a file, do 'retrieve_ncbi_record.py -l <(echo MG924986)'\n")
 
-parser.add_argument('-l', '--acc_list', type = str,
+parser.add_argument('-l', '--acc_file', type = str,
         nargs = "?", help = "file containing list of NCBI accessions (one per line)")
 
 # if no args given, print help and exit
@@ -27,17 +30,26 @@ args = parser.parse_args()
 
 # check required arguments are provided
 
-if args.acc_list is None:
+if args.acc_file is None:
     print("\n** required input missing\n"
           "** a file containing wanted accession numbers is required\n")
     parser.print_help(sys.stderr)
     sys.exit(1)
 
-acc_file = args.acc_list
+acc_handle = args.acc_file
 
-# function to fetch and write out a genbank record
+# function to fetch and a genbank record
 
 def retrieve_ncbi_record(ncbi_id):
+    print("retrieving {} from NCBI".format(ncbi_id))
     new_handle = Entrez.efetch(db="nucleotide", id=ncbi_id, rettype="gb", retmode="genbank")
     seq_record = SeqIO.read(new_handle, "genbank")
-    SeqIO.write(seq_record, ncbi_id + ".gb", "genbank")
+    return(seq_record)
+
+# get list of accession numbers and Processing
+
+acc_list = [line.strip() for line in open(acc_handle)]
+
+for acc in acc_list:
+    record = retrieve_ncbi_record(acc)
+    SeqIO.write(record, acc + ".gb", "genbank")
